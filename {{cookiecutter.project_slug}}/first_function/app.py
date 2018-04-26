e6f3618ca1023ecf05a88f85ee6d111c702594a7
Joris Conijn <jco@woodwing.com>
2018-04-26 14:48:01 +0200

    bug(first_function): Make methods more explicit and added some docs

--- {{cookiecutter.project_slug}}/first_function/app.py
+++ {{cookiecutter.project_slug}}/first_function/app.py
@@ -5,29 +5,42 @@ import os
 from aws_xray_sdk.core import xray_recorder
 
 # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
-if 'LAMBDA_TASK_ROOT' in os.environ:
+if runs_on_aws_lambda():
     from aws_xray_sdk.core import patch_all 
     patch_all()
 {%- endif %}
 
 session = boto3.Session()
 
+def runs_on_aws_lambda():
+    """
+        Returns True if this function is executed on AWS Lambda service.
+    """
+    return 'AWS_SAM_LOCAL' not in os.environ and 'LAMBDA_TASK_ROOT' in os.environ
+
 
 def lambda_handler(event, context):
+    """
+        AWS Lambda handler
+
+        This method is invoked by the API Gateway: /Prod/first/{proxy+} endpoint.
+    """
+    message = get_message()
+
 {% if cookiecutter.include_apigw == "y" %}
     return {
         "statusCode": 200,
-        "body": json.dumps(my_function())
+        "body": json.dumps(message)
     }
 {% else %}
-    return my_function()
+    return message
 {% endif %}
 
 {% if cookiecutter.include_xray == "y" -%}
 # Decorator for xray
-@xray_recorder.capture('## my_function_subsegment')
+@xray_recorder.capture('## get_message_segment')
 {% endif -%}
-def my_function():
+def get_message():
     {% if cookiecutter.include_xray == "y" -%}
     """
         You can create a sub-segment specifically to a function
@@ -36,10 +49,9 @@ def my_function():
         for example: put_annotation("operation", "query_db")
     """
     # Only run xray in the AWS Lambda environment
-    if 'AWS_SAM_LOCAL' not in os.environ and 'LAMBDA_TASK_ROOT' in os.environ:
+    if runs_on_aws_lambda():
         xray_subsegment = xray_recorder.current_subsegment()
         xray_subsegment.put_annotation("key", "value")
-
     {% endif -%}
 
     return {
