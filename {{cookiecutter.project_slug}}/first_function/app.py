e8396533b66a9c354cec39d139cb0f8e93a22f74
Joris Conijn <jco@woodwing.com>
2018-04-26 10:46:23 +0200

    bug(first_function): Fix unit tests when xray has been enabled

--- {{cookiecutter.project_slug}}/first_function/app.py
+++ {{cookiecutter.project_slug}}/first_function/app.py
@@ -2,10 +2,11 @@ import boto3
 import json
 {%- if cookiecutter.include_xray == "y" %}
 from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all 
 
 # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
-patch_all()
+if 'LAMBDA_TASK_ROOT' in os.environ:
+    from aws_xray_sdk.core import patch_all 
+    patch_all()
 {%- endif %}
 
 session = boto3.Session()
@@ -15,18 +16,18 @@ def lambda_handler(event, context):
 {% if cookiecutter.include_apigw == "y" %}
     return {
         "statusCode": 200,
-        "body": json.dumps({'hello': 'world'})
+        "body": json.dumps(my_function())
     }
 {% else %}
-    return {
-        "hello": "world"
-    }
+    return my_function()
 {% endif %}
 
 {% if cookiecutter.include_xray == "y" -%}
 # Decorator for sync function
 @xray_recorder.capture('## my_function_subsegment')
+{% endif -%}
 def my_function():
+{% if cookiecutter.include_xray == "y" -%}
     """
         You can create a sub-segment specifically to a function
         then capture what sub-segment that is inside your code
@@ -35,5 +36,7 @@ def my_function():
     """
     xray_subsegment = xray_recorder.current_subsegment()
     xray_subsegment.put_annotation("key", "value")
-    # do something
 {% endif -%}
+    return {
+        "hello": "world"
+    }
