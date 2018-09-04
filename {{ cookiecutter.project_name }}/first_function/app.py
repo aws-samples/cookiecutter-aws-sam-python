8bac5516f4e1172d07b656ad78cfdfe1f65d66a6
Heitor Lessa <lessa@amazon.com>
2018-09-04 14:03:57 +0300

    improv: Cleans up spaces, imports and add sample policies

--- {{ cookiecutter.project_name }}/first_function/app.py
+++ {{ cookiecutter.project_name }}/first_function/app.py
@@ -10,10 +10,9 @@ def runs_on_aws_lambda():
     return 'AWS_SAM_LOCAL' not in os.environ and 'LAMBDA_TASK_ROOT' in os.environ
 
 {%- if cookiecutter.include_xray == "y" %}
-from aws_xray_sdk.core import xray_recorder
-
 # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
 if runs_on_aws_lambda():
+    from aws_xray_sdk.core import xray_recorder    
     from aws_xray_sdk.core import patch_all
     patch_all()
 {%- endif %}
@@ -24,13 +23,74 @@ session = boto3.Session()
 def lambda_handler(event, context):
     """
         AWS Lambda handler
-        {% if cookiecutter.include_apigw == "y" %}
+        Parameters
+        ----------
+        context: object, required
+            Lambda Context runtime methods and attributes
 
-        This method is invoked by the API Gateway: /Prod/first/{proxy+} endpoint.
+        Attributes
+        ----------
+
+        context.aws_request_id: str
+            Lambda request ID
+        context.client_context: object
+            Additional context when invoked through AWS Mobile SDK
+        context.function_name: str
+            Lambda function name
+        context.function_version: str
+            Function version identifier
+        context.get_remaining_time_in_millis: function
+            Time in milliseconds before function times out
+        context.identity:
+            Cognito identity provider context when invoked through AWS Mobile SDK
+        context.invoked_function_arn: str
+            Function ARN
+        context.log_group_name: str
+            Cloudwatch Log group name
+        context.log_stream_name: str
+            Cloudwatch Log stream name
+        context.memory_limit_in_mb: int
+            Function memory
+
+            https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+        event: dict, required
+        {% if cookiecutter.include_apigw == "y" %}
+            API Gateway Lambda Proxy Input Format
+
+            {
+                "resource": "Resource path",
+                "path": "Path parameter",
+                "httpMethod": "Incoming request's method name"
+                "headers": {Incoming request headers}
+                "queryStringParameters": {query string parameters }
+                "pathParameters":  {path parameters}
+                "stageVariables": {Applicable stage variables}
+                "requestContext": {Request context, including authorizer-returned key-value pairs}
+                "body": "A JSON string of the request payload."
+                "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+            }
+
+            https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+        {% endif %}
+        Returns
+        ------
+        {% if cookiecutter.include_apigw == "y" %}
+        API Gateway Lambda Proxy Output Format: dict
+            'statusCode' and 'body' are required
+
+            {
+                "isBase64Encoded": true | false,
+                "statusCode": httpStatusCode,
+                "headers": {"headerName": "headerValue", ...},
+                "body": "..."
+            }
+
+            # api-gateway-simple-proxy-for-lambda-output-format
+            https: // docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
         {% endif %}
     """
     message = get_message()
-
 {% if cookiecutter.include_apigw == "y" %}
     return {
         "statusCode": 200,
@@ -56,6 +116,9 @@ def get_message():
     if runs_on_aws_lambda():
         xray_subsegment = xray_recorder.current_subsegment()
         xray_subsegment.put_annotation("key", "value")
+        # Sample metadata
+        # subsegment.put_metadata("operation", "metadata", "python object/json")
+        xray_recorder.end_subsegment()
     {% endif -%}
 
     return { "hello": "world" }
