d41de95b313d2eeaab3756d7b39ba7d639109e4b
Felipe Santos <felipemsantos@gmail.com>
2020-07-17 15:08:37 -0300

    Fix code to cover AWS Lambda Powertools features.

--- {{ cookiecutter.project_name }}/first_function/app.py
+++ {{ cookiecutter.project_name }}/first_function/app.py
@@ -2,19 +2,22 @@ import boto3
 import json
 import os
 
-{%- if cookiecutter.include_xray == "y" %}
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all  # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
+{ % - if cookiecutter.include_lambda_powertools == "y" %}
+from aws_lambda_powertools import Tracer, Logger, Metrics  # https://awslabs.github.io/aws-lambda-powertools-python/#features
 
-# Only instrument libraries if not running locally
-if "AWS_SAM_LOCAL" not in os.environ:
-    patch_all()
-{%- endif %}
+tracer = Tracer()
+logger = Logger()
+metrics = Metrics()
+{ % - endif %}
 
 # Global variables are reused across execution contexts (if available)
 session = boto3.Session()
 
-
+{ % - if cookiecutter.include_lambda_powertools == "y" %}
+@metrics.log_metrics
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
+{ % - endif %}
 def lambda_handler(event, context):
     """
         AWS Lambda handler
@@ -85,37 +88,8 @@ def lambda_handler(event, context):
             https: // docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
         {% endif %}
     """
-
-    message = get_message()
-{% if cookiecutter.include_apigw == "y" %}
-    return {
-        "statusCode": 200,
-        "body": json.dumps(message)
-    }
-{% else %}
-    return message
-{% endif %}
-
-{% if cookiecutter.include_xray == "y" -%}
-# For simple subsegments that don't need annotation/metadata you can use the decorated version
-## @xray_recorder.capture('## get_message_subsegment')
-{% endif -%}
-def get_message():
-    {% if cookiecutter.include_xray == "y" -%}
-    """
-        You can create a sub-segment specifically to a function
-        then capture what sub-segment that is inside your code
-        and you can add annotations that will be indexed by X-Ray
-        for example: put_annotation("operation", "query_db")
-    """
-    # Only run xray in the AWS Lambda environment
-    if "AWS_SAM_LOCAL" not in os.environ:
-        xray_subsegment = xray_recorder.current_subsegment()
-        xray_subsegment.put_annotation("method", "get_message") # indexed key/value data that X-Ray can use to sort traces by
-        
-        # Sample metadata - Same as annotation but non-indexed data + allows for objects/dicts
-        # subsegment.put_metadata("operation", "metadata", "python object/json")
-        xray_recorder.end_subsegment()
-    {% endif -%}
-
-    return {"hello": "world"}
+    try:
+        result = {"hello": "world"}
+    except Exception as e:
+        logger.error(e)
+        raise e
